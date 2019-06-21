94e6a404ec5e56f3b6a513855ffe41624ef53072
Paweł <pawel_wis@yahoo.com>
2019-06-21 14:32:57 +0200

    add registrarion controller

---
+++ test/controllers/registration_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class RegistrationControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get registration_index_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get registration_index_url
+    assert_response :success
+  end
+
+end
