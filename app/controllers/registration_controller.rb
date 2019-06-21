bbc1a6e71f2f645049fe6d5bb795b415f759db0f
Paweł <pawel_wis@yahoo.com>
2019-06-21 15:06:42 +0200

    Add user registration form and controller control

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,4 +1,31 @@
 class RegistrationController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
   end
+
+  def create_user
+    user = User.new email: params[:email], password: params[:password]
+
+    if user.save
+      redirect_to dashboard_path
+    else
+      flash.alert = 'Nie udało się utworzyć konta.'
+      render 'index'
+    end
+  end
+
+  def check_password_confirmation
+    if params[:password] < 6
+      flash.alert = 'Hasło jest za krótkie.'
+      render 'index'
+    elsif params[:password] != params[:password_confirmation]
+      flash.alert = 'Hasła się nie zgadzają.'
+      render 'index'
+    else
+      create_user
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/registration', to: 'registration#index'
+  post '/registration', to: 'registration#check_password_confirmation'
   get 'dashboard/', to: 'dashboard#index'
   get '/login', to: 'login#index'
   post '/login', to: 'login#check_login'
