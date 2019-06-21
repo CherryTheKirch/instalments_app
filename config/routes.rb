80f09f2eb7f44c1b3be333154c011e42b7736340
Paweł <pawel_wis@yahoo.com>
2019-06-21 14:34:25 +0200

    reorganize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/registration', to 'registration#index'
+  get '/registration', to: 'registration#index'
   get 'dashboard/', to: 'dashboard#index'
   get '/login', to: 'login#index'
   post '/login', to: 'login#check_login'
